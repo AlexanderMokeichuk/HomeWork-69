c217530af0b1c12a7b11e28eb070874efb6e016b
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-02-29 13:02:38 +0600

    Added component "Header.tsx"

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Layout: React.FC<React.PropsWithChildren> = ({children}) => {
+  return (
+    <div className={"min-vh-100 d-flex flex-column"}>
+
+    </div>
+  );
+};
+
+export default Layout;
